6da5fcb17ed9fb1066137c0cb7cab7801020e3b6
Guilherme Vieira <vieirasilvaguilherme70@gmail.com>
2025-12-07 18:40:06 -0300

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,16 @@ import express from 'express'
 import { ENV } from './config/env.js'
 import { connectDB } from './config/db.js'
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from 'inngest/express'
+
+import { inngest, functions } from './config/inngest.js'
 
 const app = express()
 
+app.use(express.json())
 app.use(clerkMiddleware())
+
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
 const startServer = async () => {
   await connectDB()
