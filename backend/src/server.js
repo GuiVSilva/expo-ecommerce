1fc750def9911a133c1e3edb5f26e7e321ae3113
Guilherme Vieira <vieirasilvaguilherme70@gmail.com>
2025-12-07 17:59:07 -0300

    db config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,12 @@
 import express from 'express'
+import { ENV } from './config/env.js'
+import { connectDB } from './config/db.js'
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express()
 
-app.listen(3000, () => console.log('Server is running on port 3000'))
+app.use(clerkMiddleware())
+
+app.listen(ENV.PORT, () => {
+  console.log('Server is running on port 3000'), connectDB()
+})
